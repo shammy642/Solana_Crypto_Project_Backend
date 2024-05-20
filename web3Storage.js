9cc73f88d88ec19b56eb0ed025fef2b2bd6e7c1d
shammy642 <samuel.hoile@gmail.com>
2024-05-20 10:09:12 +0100

    more removals...

--- web3Storage.js
+++ web3Storage.js
@@ -1,29 +1,29 @@
-import { create } from "@web3-storage/w3up-client";
+// import { create } from "@web3-storage/w3up-client";
 
-const client = await create();
+// const client = await create();
 
-export const createAndConnect = async () => {
- try {
-    await client.login(process.env.WEB3_STORAGE_EMAIL);
-    await client.setCurrentSpace(
-      process.env.WEB3_STORAGE_DID
-    )
- }
- catch (error) {
-    console.log("error logging in")
- }
-};
+// export const createAndConnect = async () => {
+//  try {
+//     await client.login(process.env.WEB3_STORAGE_EMAIL);
+//     await client.setCurrentSpace(
+//       process.env.WEB3_STORAGE_DID
+//     )
+//  }
+//  catch (error) {
+//     console.log("error logging in")
+//  }
+// };
 
-export const uploadFileToIpfs = async (filePath) => {
-    console.log(filePath)
-    try {
-      // const files = await filesFromPaths(filePath);
-      const cid = await client.uploadFile(file);
-      console.log(`https://${cid}.ipfs.w3s.link`);
-      return cid; 
-    } catch (error) {
-      console.error("Upload failed:", error);
-      return null;  
-    }
-  };
+// export const uploadFileToIpfs = async (filePath) => {
+//     console.log(filePath)
+//     try {
+//       // const files = await filesFromPaths(filePath);
+//       const cid = await client.uploadFile(file);
+//       console.log(`https://${cid}.ipfs.w3s.link`);
+//       return cid; 
+//     } catch (error) {
+//       console.error("Upload failed:", error);
+//       return null;  
+//     }
+//   };
   
